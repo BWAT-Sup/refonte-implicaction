f6002b3d7f4f8b4b9294b202baea936150100233
tina <tinhinaneachir@gmail.com>
2023-02-01 09:14:57 +0100

    test websocket

--- backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/WebSocketController.java
+++ backend-implicaction/src/main/java/com/dynonuggets/refonteimplicaction/controller/WebSocketController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Controller
+
 @CrossOrigin("*")
 public class WebSocketController {
 
